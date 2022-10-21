75481579fb9070e24c33b5d13e269eeda5d4f7b6
Yanis Bouger <yanis.bouger@gmail.com>
2022-10-21 12:08:06 +0200

    resolve merge conflict for indent

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,26 +5,26 @@ import { User } from './user.entity';
 @Controller('users')
 export class UsersController {
     constructor(
-        private service : UsersService
-    ){}
+        private service: UsersService
+    ) { }
 
     @Get()
     getAll(): User[] {
         return this.service.getAll();
     }
 
-    @Get(':id') 
-    getByID(@Param() parameter): User{
+    @Get(':id')
+    getByID(@Param() parameter): User {
         return this.service.getByID(parameter.id);
     }
 
     @Post()
-    create(@Body() input:any): User {
+    create(@Body() input: any): User {
         return this.service.create(input.lastname, input.firstname, input.age);
     }
 
     @Put(':id')
-    edit(@Param() parameter, @Body() input:any) : User {
+    edit(@Param() parameter, @Body() input: any): User {
         return this.service.edit(parameter.id, input.lastname, input.firstname, input.age);
     }
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,12 @@
-import { Injectable, Get, Body, Post, Param, Put, Delete, HttpException, HttpStatus} from '@nestjs/common';
+import { Injectable, Get, Body, Post, Param, Put, Delete, HttpException, HttpStatus } from '@nestjs/common';
 //https://bobbyhadz.com/blog/typescript-file-is-not-a-module
 import { User } from './user.entity';
 
-const users : User[] = [
+const users: User[] = [
     {
         id: 0,
-        lastname: 'Bouger',
-        firstname: 'Yanis',
+        lastname: 'Doe',
+        firstname: 'John',
         age: 23
     }
 ]
@@ -18,41 +18,39 @@ export class UsersService {
         return users;
     }
 
-    @Get(':id') 
-    getByID(@Param() id : number): User{
+    @Get(':id')
+    getByID(@Param() id: number): User {
         return users.find(user => user.id == id);
     }
 
     @Post()
-    create(@Body() lastname : string, firstname : string, age : number): User {
-        let id : number = users.length;
-        let u:User = new User(id, lastname, firstname, age);
+    create(@Body() lastname: string, firstname: string, age: number): User {
+        let id: number = users.length;
+        let u: User = new User(id, lastname, firstname, age);
         users.push(u);
         return u;
     }
 
     @Put(':id')
-    edit(@Param() id : number, @Body() lastname : string, firstname : string, age : number) : User {
-        let u : User = users.find(user => user.id == id);
+    edit(@Param() id: number, @Body() lastname: string, firstname: string, age: number): User {
+        let u: User = users.find(user => user.id == id);
         if (lastname !== undefined) {
-            u.lastname=lastname;
+            u.lastname = lastname;
         }
         if (firstname !== undefined) {
-            u.firstname=firstname;
+            u.firstname = firstname;
         }
         if (age !== undefined) {
-            u.age=age;
+            u.age = age;
         }
         return u;
     }
 
     @Delete(':id')
-    popFromUsers(@Param() id : number) {
-        let u : User = users.find(user => user.id == id);
+    popFromUsers(@Param() id: number) {
+        let u: User = users.find(user => user.id == id);
         if (u !== undefined) {
             users.splice(id, 1);
-        } else {
-            throw new HttpException(`AUCUN UTILISATEUR CORRESPONDANT AVEC L\'ID : ${id}`, HttpStatus.NOT_FOUND);
         }
     }
 }
