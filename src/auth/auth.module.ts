24d6ec8095bd3f3ac37f4249785e37de716c0605
mledouarin <marius.ledouarin@etudiant.univ-rennes1.fr>
2022-11-20 16:55:54 +0100

    commit pour ajout des modifications du prochain commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { JwtStrategy } from './jwt.stategy';
   imports: [UsersModule, PassportModule,
   JwtModule.register({
     secret: jwtConstants.secret,
-    signOptions: { expiresIn: '60s'}
+    signOptions: { expiresIn: '6000s'}
   })],
   controllers: [AuthController]
 })
